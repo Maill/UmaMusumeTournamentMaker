0d28bc4f2274e83345df0a974d07d3559dddfd94
Maill <nicolas.demoncourt@gmail.com>
2025-08-28 23:17:15 +0200

    Color and spacing standardization

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { CreateTournamentPageComponent } from './pages/create-tournament/create-tournament.page';
-import { Testingpage } from './pages/test/testingpage/testingpage';
 import { TournamentDetailPageComponent } from './pages/tournament-detail/tournament-detail.page';
 import { TournamentListPageComponent } from './pages/tournament-list/tournament-list.page';
 
@@ -9,5 +8,4 @@ export const routes: Routes = [
   { path: 'tournaments', component: TournamentListPageComponent },
   { path: 'tournaments/create', component: CreateTournamentPageComponent },
   { path: 'tournaments/:id', component: TournamentDetailPageComponent },
-  { path: 'testing/testing', component: Testingpage },
 ];
